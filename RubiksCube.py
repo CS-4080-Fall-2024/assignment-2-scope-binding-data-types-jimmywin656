639bf673961b647a960e25d1ea0f44bde1ecd567
Jimmy Nguyen <jimmyn6565@gmail.com>
2024-10-23 20:07:20 -0700

    Updated Rubiks Cube Assignment

--- RubiksCube.py
+++ RubiksCube.py
@@ -1,73 +1,48 @@
-# Class that represents a face of a Rubik's cube
 class Face:
     def __init__(self, size, color):
-        # Initialize a face with a grid of size 'size' and set it as color
+        # initialize a face with all cells of size x size = color
         self.grid = [[color] * size for _ in range(size)]
 
-    # Rotate face either clockwise (cw) or counterclockwise (ccw), default is clockwise
-    def rotate(self, counter=False):
-        if counter:
-            self.grid = self.rotate_ccw()  # Call counterclockwise rotation if specified
-        else:
-            self.grid = self.rotate_cw()    # Otherwise, rotate clockwise
-
-    def rotate_cw(self):
-        # Rotate face 90 degrees clockwise
-        return [list(reversed(col)) for col in zip(*self.grid)]
-
-    def rotate_ccw(self):
-        # Rotate face 90 degrees counterclockwise
-        return [list(col) for col in reversed(list(zip(*self.grid)))]
-
-    # Getter for the bottom row of the face
-    def get_bot_row(self):
-        return self.grid[-1][:]
-
-    # Getter for the top row of the face
+    # limit rotation to only clockwise
+    # if user wants ccw then we will do clockwise 3x
+    def rotate(self):
+        self.grid = [list(reversed(col)) for col in zip(*self.grid)]
+    
+    # FACE GETTERS
+    def get_rightmost_col(self):
+        rightmost_col = [self.grid[i][-1] for i in range(len(self.grid))]
+        return list((rightmost_col))
+    def get_leftmost_col(self):
+        leftmost_col = [self.grid[i][0] for i in range(len(self.grid))]
+        return list((leftmost_col))
     def get_top_row(self):
-        return self.grid[0][:]
-
-    # Getter for the left column of the face
-    def get_left_col(self):
-        return [self.grid[i][0] for i in range(len(self.grid))]
-
-    # Getter for the right column of the face
-    def get_right_col(self):
-        return [self.grid[i][-1] for i in range(len(self.grid))]
-
-    # Setter for the bottom row of the face
-    def set_bot_row(self, row):
-        self.grid[-1] = row[:]
-
-    # Setter for the top row of the face
+        return self.grid[0]
+    def get_bot_row(self):
+        return self.grid[-1]
+    
+    # FACE SETTERS
+    def set_rightmost_col(self, right_col):
+        for i in range(len(self.grid)):
+            self.grid[i][-1] = right_col[i]
+    def set_leftmost_col(self, left_col):
+        for i in range(len(self.grid)):
+            self.grid[i][0] = left_col[i]
     def set_top_row(self, row):
-        self.grid[0] = row[:]
+        self.grid[0] = row
+    def set_bot_row(self, row):
+        self.grid[-1] = row
 
-    # Setter for the left column of the face
-    def set_left_col(self, col):
-        for i in range(len(self.grid)):
-            self.grid[i][0] = col[i]
-
-    # Setter for the right column of the face
-    def set_right_col(self, col):
-        for i in range(len(self.grid)):
-            self.grid[i][-1] = col[i]
-
-
-# Class that represents a Rubik's cube
 class RubiksCube:
-    # Color constants for each face of the cube
+    # CONSTANT COLORS
     WHITE = "W"
     RED = "R"
     BLUE = "B"
     GREEN = "G"
-    ORANGE = "O"
+    ORANGE  = "O"
     YELLOW = "Y"
 
     def __init__(self, size):
-        # Initialize the size of Rubik's cube (this determines the grid size of each face)
         self.size = size
-        # Initialize the cube faces with their respective colors
         self.faces = {
             "Up": Face(size, self.WHITE),
             "Down": Face(size, self.YELLOW),
@@ -76,135 +51,130 @@ class RubiksCube:
             "Left": Face(size, self.GREEN),
             "Right": Face(size, self.BLUE)
         }
+        # adjacent rows of bottom, left, top, right adjacent faces of given face
+        self.face_adjacents = {
+            "Up": ["Front", "Left", "Back", "Right"],
+            "Down": ["Back", "Left", "Front", "Right"],
+            "Front": ["Down", "Left", "Up", "Right"],
+            "Back": ["Down", "Right", "Up", "Left"],
+            "Left": ["Down", "Back", "Up", "Front"],
+            "Right": ["Down", "Front", "Up", "Back"]
+        }
 
-    def rotate_face(self, face, clockwise=True):
-        # Rotate the specified face of the cube
-        if face in self.faces:
-            self.faces[face].rotate(counter=not clockwise)  # Rotate the face based on direction
-        else:
-            raise ValueError("Face does not exist")  # Raise an error if the face is invalid
-        
-        # Call appropriate function to update adjacent edges based on the rotated face
-        if face == "Front":
-            self.rotate_edges_front(clockwise)
-        elif face == "Back":
-            self.rotate_edges_back(clockwise)
-        elif face == "Up":
-            self.rotate_edges_up(clockwise)
-        elif face == "Down":
-            self.rotate_edges_down(clockwise)
-        elif face == "Left":
-            self.rotate_edges_left(clockwise)
-        elif face == "Right":
-            self.rotate_edges_right(clockwise)
+    # dictionary to map the corresponding functions based on a given "face"
+    # this returns the relative row of the adjacent faces that we need to manipulate/move to other adjacent 
+    # sides uninvolved with the main face (this is to edit the surrounding faces not the current "face")
+    def get_adjacent_face_functions(self, face):
+        face_function_map = {
+            "Up": [
+                lambda f: self.faces[f].get_top_row(),
+                lambda f: self.faces[f].get_top_row(),
+                lambda f: self.faces[f].get_top_row(),
+                lambda f: self.faces[f].get_top_row()
+            ],
+            "Down": [
+                lambda f: self.faces[f].get_bot_row(),
+                lambda f: self.faces[f].get_bot_row(),
+                lambda f: self.faces[f].get_bot_row(),
+                lambda f: self.faces[f].get_bot_row()
+            ],
+            "Front": [
+                lambda f: self.faces[f].get_top_row(),
+                lambda f: self.faces[f].get_rightmost_col(),
+                lambda f: self.faces[f].get_bot_row(),
+                lambda f: self.faces[f].get_leftmost_col()
+            ],
+            "Back": [
+                lambda f: self.faces[f].get_bot_row(),
+                lambda f: self.faces[f].get_rightmost_col(),
+                lambda f: self.faces[f].get_top_row(),
+                lambda f: self.faces[f].get_leftmost_col()
+            ],
+            "Left": [
+                lambda f: self.faces[f].get_leftmost_col(),
+                lambda f: self.faces[f].get_rightmost_col(),
+                lambda f: self.faces[f].get_leftmost_col(),
+                lambda f: self.faces[f].get_leftmost_col()
+            ],
+            "Right": [
+                lambda f: self.faces[f].get_rightmost_col(),
+                lambda f: self.faces[f].get_rightmost_col(),
+                lambda f: self.faces[f].get_rightmost_col(),
+                lambda f: self.faces[f].get_leftmost_col()
+            ]
+        }
+        return face_function_map.get(face, [])
+    
+    # some rows are reversed due to the way I print and hold the cube on certain faces
+    def set_adj_face_functions(self, face):
+        face_function_map = {
+            "Up": [
+                lambda f, row: self.faces[f].set_top_row(row),
+                lambda f, row: self.faces[f].set_top_row(row),
+                lambda f, row: self.faces[f].set_top_row(row),
+                lambda f, row: self.faces[f].set_top_row(row)
+            ],
+            "Down": [
+                lambda f, row: self.faces[f].set_bot_row(row),
+                lambda f, row: self.faces[f].set_bot_row(row),
+                lambda f, row: self.faces[f].set_bot_row(row),
+                lambda f, row: self.faces[f].set_bot_row(row)
+            ],
+            "Front": [
+                lambda f, row: self.faces[f].set_top_row(list(reversed(row))),
+                lambda f, row: self.faces[f].set_rightmost_col(row),
+                lambda f, row: self.faces[f].set_bot_row(list(reversed(row))),
+                lambda f, row: self.faces[f].set_leftmost_col(row)
+            ],
+            "Back": [
+                lambda f, row: self.faces[f].set_bot_row(row),
+                lambda f, row: self.faces[f].set_rightmost_col(list(reversed(row))),
+                lambda f, row: self.faces[f].set_top_row(row),
+                lambda f, row: self.faces[f].set_leftmost_col(list(reversed(row)))
+            ],
+            "Left": [
+                lambda f, row: self.faces[f].set_leftmost_col(row),
+                lambda f, row: self.faces[f].set_rightmost_col(list(reversed(row))),
+                lambda f, row: self.faces[f].set_leftmost_col(list(reversed(row))),
+                lambda f, row: self.faces[f].set_leftmost_col(row)
+            ],
+            "Right": [
+                lambda f, row: self.faces[f].set_rightmost_col(list(reversed(row))),
+                lambda f, row: self.faces[f].set_rightmost_col(row),
+                lambda f, row: self.faces[f].set_rightmost_col(row),
+                lambda f, row: self.faces[f].set_leftmost_col(list(reversed(row)))
+            ]
+        }
+        return face_function_map.get(face, [])
 
-    def rotate_edges_front(self, clockwise):
-        """Updates the edges when rotating the front face."""
-        if clockwise:
-            # Store the bottom row of the Up face to rotate
-            top_row = self.faces["Up"].get_bot_row()
-            # Update edges accordingly
-            self.faces["Up"].set_bot_row(self.faces["Left"].get_right_col()[::-1])
-            self.faces["Left"].set_right_col(self.faces["Down"].get_top_row())
-            self.faces["Down"].set_top_row(self.faces["Right"].get_left_col()[::-1])
-            self.faces["Right"].set_left_col(top_row)
-        else:
-            # Perform counterclockwise edge updates
-            top_row = self.faces["Up"].get_bot_row()
-            self.faces["Up"].set_bot_row(self.faces["Right"].get_left_col())
-            self.faces["Right"].set_left_col(self.faces["Down"].get_top_row()[::-1])
-            self.faces["Down"].set_top_row(self.faces["Left"].get_right_col())
-            self.faces["Left"].set_right_col(top_row[::-1])
+    # rotates a face on the rubik's cube and adjusts all other faces accordingly
+    def rotate_face_cw(self, face):
+        # rotate the current face
+        self.faces[face].rotate()
 
-    # Updating the adjacent edges when rotating the back face
-    def rotate_edges_back(self, clockwise):
-        if clockwise:
-            # Store the top row of the Up face to rotate
-            top_row = self.faces["Up"].get_top_row()
-            # Update edges accordingly
-            self.faces["Up"].set_top_row(self.faces["Right"].get_right_col())
-            self.faces["Right"].set_right_col(self.faces["Down"].get_bot_row()[::-1])
-            self.faces["Down"].set_bot_row(self.faces["Left"].get_left_col())
-            self.faces["Left"].set_left_col(top_row[::-1])
-        else:
-            # Perform counterclockwise edge updates
-            top_row = self.faces["Up"].get_top_row()
-            self.faces["Up"].set_top_row(self.faces["Left"].get_left_col()[::-1])
-            self.faces["Left"].set_left_col(self.faces["Down"].get_bot_row())
-            self.faces["Down"].set_bot_row(self.faces["Right"].get_right_col()[::-1])
-            self.faces["Right"].set_right_col(top_row)
+        # gets adjacent face names
+        adjacent_faces = self.face_adjacents[face]
+        # determine what set of functions we need depending on "face"
+        get_face_functions = self.get_adjacent_face_functions(face)
 
-    # Updating the adjacent edges when rotating the up face
-    def rotate_edges_up(self, clockwise):
-        if clockwise:
-            # Store the top row of the Front face to rotate
-            front_row = self.faces["Front"].get_top_row()
-            # Update edges accordingly
-            self.faces["Front"].set_top_row(self.faces["Right"].get_top_row())
-            self.faces["Right"].set_top_row(self.faces["Back"].get_bot_row()[::-1])
-            self.faces["Back"].set_bot_row(self.faces["Left"].get_top_row()[::-1])
-            self.faces["Left"].set_top_row(front_row)
-        else:
-            # Perform counterclockwise edge updates
-            front_row = self.faces["Front"].get_top_row()
-            self.faces["Front"].set_top_row(self.faces["Left"].get_top_row())
-            self.faces["Left"].set_top_row(self.faces["Back"].get_bot_row()[::-1])
-            self.faces["Back"].set_bot_row(self.faces["Right"].get_top_row()[::-1])
-            self.faces["Right"].set_top_row(front_row)
+        # get rows of interest from adjacent faces
+        adjacent_rows = [get_face_functions[i](adjacent_faces[i]) for i in range(len(adjacent_faces))]
+        # Rotate adjacent rows in circular fashion 
+        # (the rows taken need to move to the next row in line because of the clockwise spin)
+        new_rows = adjacent_rows[-1:] + adjacent_rows[:-1]  # Right shift
 
-    # Updating the adjacent edges when rotating the down face
-    def rotate_edges_down(self, clockwise):
-        if clockwise:
-            # Store the bottom row of the Front face to rotate
-            front_row = self.faces["Front"].get_bot_row()
-            # Update edges accordingly
-            self.faces["Front"].set_bot_row(self.faces["Left"].get_bot_row())
-            self.faces["Left"].set_bot_row(self.faces["Back"].get_top_row()[::-1])
-            self.faces["Back"].set_top_row(self.faces["Right"].get_bot_row()[::-1])
-            self.faces["Right"].set_bot_row(front_row)
-        else:
-            # Perform counterclockwise edge updates
-            front_row = self.faces["Front"].get_bot_row()
-            self.faces["Front"].set_bot_row(self.faces["Right"].get_bot_row())
-            self.faces["Right"].set_bot_row(self.faces["Back"].get_top_row()[::-1])
-            self.faces["Back"].set_top_row(self.faces["Left"].get_bot_row()[::-1])
-            self.faces["Left"].set_bot_row(front_row)
+        # define setter functions for each face
+        set_face_functions = self.set_adj_face_functions(face)
 
-    # Updating the adjacent edges when rotating the left face
-    def rotate_edges_left(self, clockwise):
-        if clockwise:
-            # Store the left column of the Front face to rotate
-            front_col = self.faces["Front"].get_left_col()
-            # Update edges accordingly
-            self.faces["Front"].set_left_col(self.faces["Up"].get_left_col())
-            self.faces["Up"].set_left_col(self.faces["Back"].get_right_col()[::-1])
-            self.faces["Back"].set_right_col(self.faces["Down"].get_left_col()[::-1])
-            self.faces["Down"].set_left_col(front_col)
-        else:
-            # Perform counterclockwise edge updates
-            front_col = self.faces["Front"].get_left_col()
-            self.faces["Front"].set_left_col(self.faces["Down"].get_left_col())
-            self.faces["Down"].set_left_col(self.faces["Back"].get_right_col()[::-1])
-            self.faces["Back"].set_right_col(self.faces["Up"].get_left_col()[::-1])
-            self.faces["Up"].set_left_col(front_col)
+        # loop through adjacent_faces and new_rows, applying the setters
+        for i in range(len(adjacent_faces)):
+            set_face_functions[i](adjacent_faces[i], new_rows[i])
 
-    # Updating the adjacent edges when rotating the right face
-    def rotate_edges_right(self, clockwise):
-        if clockwise:
-            # Store the right column of the Front face to rotate
-            front_col = self.faces["Front"].get_right_col()
-            # Update edges accordingly
-            self.faces["Front"].set_right_col(self.faces["Down"].get_right_col())
-            self.faces["Down"].set_right_col(self.faces["Back"].get_left_col())
-            self.faces["Back"].set_left_col(self.faces["Up"].get_right_col()[::-1])
-            self.faces["Up"].set_right_col(front_col[::-1])
-        else:
-            # Perform counterclockwise edge updates
-            front_col = self.faces["Front"].get_right_col()
-            self.faces["Front"].set_right_col(self.faces["Up"].get_right_col())
-            self.faces["Up"].set_right_col(self.faces["Back"].get_left_col()[::-1])
-            self.faces["Back"].set_left_col(self.faces["Down"].get_right_col()[::-1])
-            self.faces["Down"].set_right_col(front_col)
+    # counter clockwise on a rubiks cube is = to 3 clockwise rotations
+    def rotate_face_ccw(self, face):
+        self.rotate_face_cw(face)
+        self.rotate_face_cw(face)
+        self.rotate_face_cw(face)
 
     # display rubiks cube
     def display(self):
@@ -215,15 +185,26 @@ class RubiksCube:
             print()
 
 
-# Example usage
 cube = RubiksCube(3)
-# cube.display()
 
 # Test the following moves
-cube.rotate_face("Front")
-cube.rotate_face("Right")
-cube.rotate_face("Up")
-# cube.rotate_face("Up")
-# cube.rotate_face("Left")
-# cube.rotate_face("Down", clockwise=False)
+# clockwise spin on a rubiks cube is a spin to the right
+# counterclockwise spin is a spin to the left (when holding the face towards you)
+cube.rotate_face_cw("Front")
+cube.rotate_face_cw("Up")
+cube.rotate_face_cw("Down")
+cube.rotate_face_cw("Left")
+cube.rotate_face_cw("Left")
+cube.rotate_face_cw("Right")
+cube.rotate_face_cw("Back")
+cube.rotate_face_ccw("Front")   # counter clockwise
+
+# HOW THE CUBE FACES ARE DISPLAYED
+###############################################################################
+# FRONT = RED side (WHITE above, YELLOW below, GREEN left, BLUE right)
+# UP = WHITE side (ORANGE, above, RED below, GREEN left, BLUE right)
+# DOWN = YELLOW side (RED above, ORANGE below, GREEN left, BLUE right)
+# LEFT = GREEN side (WHITE above, YELLOW below, ORANGE left, RED right)
+# RIGHT = BLUE side (WHITE above, YELLOW below, RED left, ORANGE right)
+# BACK = ORANGE side (WHITE above, YELLOW below, BLUE left, GREEN right)
 cube.display()
